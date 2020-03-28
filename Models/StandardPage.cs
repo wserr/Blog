0b441847d1b2ec49511d0dbcdd1084f3ed99739e
Willem Serruys <serruys.willem@hotmail.com>
2020-03-28 18:13:50 +0100

    Initial Commit

---
+++ Models/StandardPage.cs
@@ -0,0 +1,10 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace Blog.Models
+{
+    [PageType(Title = "Standard page")]
+    public class StandardPage  : Page<StandardPage>
+    {
+    }
+}
