0ede5f04093cae24b8879a42145667ad3db685e2
Willem Serruys <serruys.willem@hotmail.com>
2020-05-14 21:02:18 +0200

    changes menu items

---
+++ Models/NavigationConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models
+{
+    public class NavigationConfiguration
+    {
+        public List<PageConfiguration> Pages { get; set; } = new List<PageConfiguration>();
+        public List<OverviewConfiguration> Overviews { get; set; } = new List<OverviewConfiguration>();
+    }
+
+    public class OverviewConfiguration
+    {
+        public string Name { get; set; }
+        public string MetaKeyword { get; set; }
+    }
+
+    public class PageConfiguration
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
